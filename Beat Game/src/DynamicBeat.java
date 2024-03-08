bdd539ad3d7fb4e1f993e0b44d2f6c2dec97e9d0
ijunsoo <159103870+ijunsoo@users.noreply.github.com>
2024-03-08 13:01:25 +0900

    자바 리듬게임 프로젝트 생성

---
+++ Beat Game/src/DynamicBeat.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class DynamicBeat extends JFrame {
+    public DynamicBeat(){
+        setTitle("BeatGame");
+        setSize(Main.SCREEN_WIDTH,Main.SCREEN_HEIGHT);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+}
---
+++ Beat Game/src/Main.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the
+public class Main {
+    public static final int SCREEN_WIDTH =1280;
+    public static final int SCREEN_HEIGHT =720;
+    public static void main(String[] args) {
+        new DynamicBeat();
+}
+}
